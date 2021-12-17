31848821c80ebe7beae88195603498ef574cd60b
robinsonjesus <tomyagarcia@hotmail.com>
2021-12-16 23:13:44 -0300

    third commit

--- dvg/backend/backend/settings.py
+++ dvg/backend/backend/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%lg%eiyg(#cxj*amu+s==%kl)zet3ko_l*3^f(3)z1i@%i3p5('
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
